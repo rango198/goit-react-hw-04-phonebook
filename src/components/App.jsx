aaa6a75698deec2e62aaec2a4405736c3ecd60e6
rango198 <137562811+rango198@users.noreply.github.com>
2024-01-20 11:14:01 +0200

    fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import initialContacts from './contacts.json';
 import { ContactsList } from './ContactsList/ContactsList';
 import { Filter } from './filter/filter';
@@ -6,70 +6,64 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { ContactsForm } from './Form/ContactsForm';
 import { ContactsWrap, Container, PhoneWrap } from './App.styled';
 
-export class App extends Component {
-  state = {
-    contacts: initialContacts,
-    filter: '',
-  };
+export const App = () => {
+  const [contacts, setContacts] = useState(initialContacts);
+  const [filter, setFilter] = useState('');
+  // state = {
+  //   contacts: initialContacts,
+  //   filter: '',
+  // };
 
-  addContacts = newContact => {
-    this.state.contacts.filter(
+  const addContacts = newContact => {
+    const isContactsExist = contacts.some(
       contact =>
         contact.name.toLowerCase().trim() ===
           newContact.name.toLowerCase().trim() ||
         contact.number.trim() === newContact.number.trim()
-    ).length
-      ? Notify.warning(`${newContact.name}: is already in contacts`, {
-          width: '400px',
-          position: 'center-center',
-          timeout: 3000,
-          fontSize: '20px',
-        })
-      : this.setState(prevState => {
-          return {
-            contacts: [newContact, ...prevState.contacts],
-          };
-        });
+    );
+    if (isContactsExist) {
+      Notify.warning(`${newContact.name}: is already in contacts`, {
+        width: '400px',
+        position: 'center-center',
+        timeout: 3000,
+        fontSize: '20px',
+      });
+    } else {
+      setContacts(prevContacts => [newContact, ...prevContacts]);
+    }
   };
 
-  onChangeFilter = event => {
+  const onChangeFilter = event => {
     const { value } = event.currentTarget;
-    this.setState({ filter: value });
+    setFilter(value);
   };
 
-  filterByName = () => {
-    const { contacts, filter } = this.state;
+  const filterByName = () => {
     const lowerFilter = filter.toLowerCase();
     return contacts.filter(({ name }) =>
       name.toLowerCase().includes(lowerFilter)
     );
   };
 
-  onDeleteContacts = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-    }));
+  const onDeleteContacts = contactId => {
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== contactId)
+    );
   };
 
-  render() {
-    const visibleContacts = this.filterByName();
-    const { filter } = this.state;
+  const visibleContacts = filterByName();
 
-    return (
-      <Container>
-        <PhoneWrap>
-          <h1>Phonebook</h1>
-          <ContactsForm addContacts={this.addContacts} />
-        </PhoneWrap>
-        <ContactsWrap>
-          <h2>Contacts</h2>
-          <Filter onChangeFilter={this.onChangeFilter} filter={filter} />
-          <ContactsList
-            contacts={visibleContacts}
-            onDelete={this.onDeleteContacts}
-          />
-        </ContactsWrap>
-      </Container>
-    );
-  }
-}
+  return (
+    <Container>
+      <PhoneWrap>
+        <h1>Phonebook</h1>
+        <ContactsForm addContacts={addContacts} />
+      </PhoneWrap>
+      <ContactsWrap>
+        <h2>Contacts</h2>
+        <Filter onChangeFilter={onChangeFilter} filter={filter} />
+        <ContactsList contacts={visibleContacts} onDelete={onDeleteContacts} />
+      </ContactsWrap>
+    </Container>
+  );
+};
